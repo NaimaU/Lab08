dd3bb0170ebfc75f82b936e302184371b8b51389
NaimaU <naimaulusow42@gmail.com>
2025-10-30 17:27:34 -0600

    test delete 1

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -12,4 +12,38 @@ public class CustomListTest {
         assertTrue(list.hasCity(calgary));
     }
 
+    @Test
+    void testDelete() {
+        CityList cityList = mockCityList();
+        assertEquals(1, cityList.getCities().size());
+
+        City city = new City("Regina", "Saskatchewan");
+        cityList.add(city);
+        assertEquals(2, cityList.getCities().size());
+
+        cityList.delete(city);
+        assertEquals(1, cityList.getCities().size());
+
+        // throw an exception if a city deleted is not in the list
+        assertThrows(IllegalArgumentException.class, () -> {
+            cityList.delete(city);
+        });
+    }
+
+//    @Test
+//    void testCountCities() {
+//        CityList cityList = mockCityList();
+//        assertEquals(1, cityList.countCities());
+//
+//        // Create another city object
+//        City city = new City("Charlottetown", "Prince Edward Island");
+//        cityList.add(city);
+//        assertEquals(2, cityList.countCities());
+//
+//        // delete said city object
+//        cityList.delete(city);
+//        assertEquals(1, cityList.countCities());
+//    }
+}
+
 }
