64ec43892bce5447ec47aa67473669bf494f4c60
NaimaU <naimaulusow42@gmail.com>
2025-10-30 17:21:01 -0600

    committing failing test

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -10,12 +10,24 @@ public class CustomList {
         this.cities = new ArrayList<>();
     }
 
+    /**
+     * this adds a city object to the list
+     * the second phase, you can add the city
+     * @param city
+     */
     public void addCity(City city) {
-        cities.add(city);
+        //cities.add(city);
     }
+
+    public int getCount(){
+        return cities.size();
+    }
+
 
     
 
     // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
+//    public boolean hasCity(City city) {
+//
+//    }
 }
